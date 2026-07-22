ab8efcbfc7f1bc87c73d75eaa164f53278a99e74
Aniket Kumr Singh <singhanita06798@gmail.com>
2026-07-22 11:12:20 +0530

    add function

---
+++ lab1/index2.js
@@ -0,0 +1,24 @@
+// write a function to take number between 0 to 9 and return in words
+
+
+const numberToWord = (num) => {
+    const words = [
+        "Zero", "One", "Two", "Three", "Four",
+        "Five", "Six", "Seven", "Eight", "Nine"
+    ];
+
+    return (num >= 0 && num <= 9) ? words[num] : "Invalid Number";
+};
+
+console.log(numberToWord(0));
+console.log(numberToWord(1));
+console.log(numberToWord(2));
+console.log(numberToWord(3));
+console.log(numberToWord(4));
+console.log(numberToWord(5)); 
+console.log(numberToWord(6));
+console.log(numberToWord(7));
+console.log(numberToWord(8));
+console.log(numberToWord(9)); 
+
+console.log(numberToWord(11)); 
