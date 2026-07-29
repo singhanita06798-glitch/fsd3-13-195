8c81e3ca92f339b99884c6414efa24cd9cd8a9c7
Aniket Kumr Singh <singhanita06798@gmail.com>
2026-07-29 12:06:09 +0530

    file delete

--- lab2/file3.js
+++ lab2/file3.js
@@ -1,4 +1,5 @@
-import { readFile, writeFile } from "fs/promises";
+import { readFile, writeFile ,appendFile,unlink} from "fs/promises";
+
 const writeData = async(fname,contents)=>{
     await writeFile(fname,contents);
     console.log('File written');
@@ -14,8 +15,13 @@ const appendData= async(fname,contents)=>{
     await appendFile(fname,"\n"+ contents)
 };
 
+const deleteFile=async(fname)=>{
+    await unlink(fname);
+    console.log(`${fanme} deleted`);
+};
 
-await writeData("happy.txt","I am very happy");
-await readData("happy.txt");
-await appendData("happy.txt","FSD is interesting");
-await readData("happy.txt");
+// await writeData("happy.txt","I am very happy");
+// await readData("happy.txt");
+// await appendData("happy.txt","FSD is interesting");
+// await readData("happy.txt");
+await deleteFile("happy.txt");
