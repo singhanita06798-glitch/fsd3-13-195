8435d0fab35bfe48d968fc1a62ef708c5e2a9296
Aniket Kumr Singh <singhanita06798@gmail.com>
2026-09-16 12:17:59 +0530

    add update put request

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -7,6 +7,12 @@ const server = http.createServer((req,res)=> {
     }
     else if (req.url === '/' && req.method === 'POST'){
         //console.log("Request:",req)
+        
+    }
+    
+     else if (req.url.startsWith("/products/") && req.method === 'PUT'){
+        const productID = req.url.split('/').pop();
+        console.log('Update Product id:',productID);
         let body=' '
         req.on('data',(chunk)=>{
             body+= chunk
@@ -14,16 +20,18 @@ const server = http.createServer((req,res)=> {
         req.on("end",()=>{
             const product=JSON.parse(body);
             console.log("Recevied product:",product);
-            res.statusCode = 201;
-        res.end(JSON.stringify({msg:'product added',product}));
+            res.statusCode = 200;
+        res.end(JSON.stringify({msg:'product updated ',product}));
     
         });
-    }
+
+
     
-     else if (req.url === '/' && req.method === 'PUT'){
-        res.statusCode = 200;
-        res.end("PUT Request");
     }
+
+
+
+
      else if (req.url === '/' && req.method === 'DELETE'){
         res.statusCode = 200;
         res.end("DELETE Request");
